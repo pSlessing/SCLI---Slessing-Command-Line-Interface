7e6ff75309ae0dce032110a92ab173e8a565d86f
pSlessing <psless23@student.aau.dk>
2025-02-22 16:17:46 +0100

    Added documentation for usage in the README and commands for viewing earlier semester projects

--- Code/ProjectCommands.cs
+++ Code/ProjectCommands.cs
@@ -2,21 +2,40 @@ partial class CliHandler
 {
     private void Project1Semester()
     {
-
+        Console.WriteLine(@"This project was written in C, and it's purpose, was from a graph, where nodes had a certain popularity,
+        and edges had a certain amount of traffic, to find the most optimal point to place electric charging stations
+        via calculating how much traffic each stop could attract from nearby roads, due to a higher popularity.
+        Press enter to continue to the Github Repository");
+        Console.ReadLine();
+        OpenBrowser("https://github.com/cs-23-sw-1-p1-17/ChargepointPlacementFinder");
     }
 
     private void Project2Semester()
     {
-        
+        Console.WriteLine(@"For my second semester, we implemented a recommendation system for groups of people, calculating which activity
+        would be most suited for a certain group of people. While the server is implemented in Node.js, all of the calculation
+        of vectors and optimization, is done in a seperate Python script, which Node calls, while data for ratings and users are stored and retrieved
+        from an SQL database
+        Press enter to continue to the Github Repository");
+        Console.ReadLine();
+        OpenBrowser("https://github.com/Group-Project-CS-23-P2/P2");
     }
 
     private void Project3Semester()
     {
-        
+        Console.WriteLine(@"For the third semester project, we collaborated with a danish porto company, as they needed a new system for time registration.
+        This semester, a lot of the focus was on modelling, and following certain workflows, as well as a focus on the frontend design.
+        For implementation, we used Spring to implement our backend resource server, integrating with our partners authentication server, while our frontend was hosted
+        seperately for easier swapability of the backend, as that was something our partner could wish to do at a later point.
+        Press enter to continue to a copy of the Github Repository");
+        Console.ReadLine();
+        OpenBrowser("https://github.com/pSlessing/ThirdSemesterProject");
     }
 
     private void Project4Semester()
     {
-        
+        Console.WriteLine(@"This is currently WIP, but our goal is to design and implement a programming language
+        that targets the original Nintendo Gameboy, compiling from our own language, PENGUIN, to Rednex GameBoy Assembly.
+        From there, we'll use the RGBDS toolchain to compile into ROM files.");
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
+# Slessing Command Line Interface - SCLI
+
 This is a small project to make my life easier and less annoying during University!
 
 Currently, it simply opens my books and courses for different courses, and some resources for the semester project, as well as play music on TIDAL, but I'll slowly chip away at more features!
 (Also i will probably learn markdown at some point, but uhh, we'll get to it when we do)
+
+Commands:
+    - SS // Opens relevant Moodle pages and books for the course
+    - SPO // Opens relevant Moodle pages and books for the course
+    - CAOS // Opens relevant Moodle pages and books for the course
+    - JAZZ // Opens TIDAL, sets the Jazz Rap 101 playlist as the one to play, and starts the music via media keys
+
+    - Projects // Prompts for which project to display a small paragraph about it, and then opens the Github repository for said project
+        - 1
+        - 2
+        - 3
+        - 4
